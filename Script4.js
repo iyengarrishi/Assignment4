79a90ddbb65caa241eff80a250a4652a15dad438
Rishi <ri2195@columbia.edu>
2014-02-21 03:41:17 -0500

    Created Document Ready and bookSort function

--- Script4.js
+++ Script4.js
@@ -5,3 +5,20 @@
 var test= "This is assignment 4"
 
 console.log(test);
+
+//Here I begin assignment 4B
+
+// I created a JSON file out of my list from assignment 3 and named it bookList.json
+//Hopefully you'll see it here soon.
+
+//Now that jQuery has been loaded, I will create a $(document).ready function
+
+//Now I will create a function within document ready, and call it bookSort
+
+function bookSort(){
+	
+	console.log ("If I did everything right, I will see this.")
+	
+} 
+
+$(document).ready(bookSort);  
