93ef435f7477ef9ad0c5f690f03f53adac0031e3
Rishi <ri2195@columbia.edu>
2014-02-21 04:38:27 -0500

    Created a div using jQuery

--- Script4.js
+++ Script4.js
@@ -19,6 +19,16 @@ function bookSort(){
 	
 	console.log ("If I did everything right, I will see this.")
 	
+	//Creating a div using jQuery, let's call it divDiv 
+	// $ is the operator used to indicate jQuery
+	
+	var divDiv = $("<div>");
+	$(divDiv).html("I don't know why I div that");
+	//in order to make it show on the html page, I will now pull the div id from there and append it
+	$("#Main").append(divDiv);
+	
+	//Success! Forgot to append initially but fixed it
+	
 } 
 
 $(document).ready(bookSort);  
